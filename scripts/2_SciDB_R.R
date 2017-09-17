c3abd013117c62a90e9550a971ce202f7e19f9e4
Marius Appel <marius.appel@uni-muenster.de>
2017-09-17 11:35:12 +0200

    Hands-on session scripts updated

---
+++ scripts/2_SciDB_R.R
@@ -0,0 +1,10 @@
+# Example script to demonstrate the use of the scidb R package
+
+library(scidb)
+
+
+# create a proxy object
+x = scidb("S2_OKAVANGO_S")
+x
+
+
---
+++ scripts/3_scidbst.R
@@ -0,0 +1,9 @@
+# Example script to demonstrate the use of the scidbst R package
+
+
+
+@proxy
+
+
+
+@proxy@name 
